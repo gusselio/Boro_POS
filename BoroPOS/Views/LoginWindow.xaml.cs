0a26de472a1524408aed45e620830adec1088ec5
Gustavo Leal <guselio98@gmail.com>
2026-03-09 19:54:21 -0600

    Fase final de diseño de login

---
+++ BoroPOS/Views/LoginWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BoroPOS.Views
+{
+    /// <summary>
+    /// Lógica de interacción para LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BarraTitulo_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void SoloNumeros(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(char.IsDigit);
+        }
+
+        private void Minimizar_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Cerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private bool passwordVisible = false;
+
+        private void MostrarPassword_Click(object sender, RoutedEventArgs e)
+        {
+            if (!passwordVisible)
+            {
+                txtPasswordVisible.Text = txtPasswordOculto.Password;
+                txtPasswordOculto.Visibility = Visibility.Collapsed;
+                txtPasswordVisible.Visibility = Visibility.Visible;
+                passwordVisible = true;
+            }
+            else
+            {
+                txtPasswordOculto.Password = txtPasswordVisible.Text;
+                txtPasswordVisible.Visibility = Visibility.Collapsed;
+                txtPasswordOculto.Visibility = Visibility.Visible;
+                passwordVisible = false;
+            }
+        }
+    }
+}
